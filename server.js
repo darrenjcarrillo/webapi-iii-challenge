11c3400e9823f8e64951188d8d130e4a9bc31aec
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-10-30 13:10:31 -0700

    Step 1

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
-const express = 'express';
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const server = express();
-
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
-});
+// import custom middleware
+// const validateUserId = require()
+// const validateUser
+// const validatePost
 
 //custom middleware
 
-function logger(req, res, next) {
+function logger(prefix) {
+  return (req, res, next) => {
+    console.log(
+      `LOGGER IS ON ${prefix} ${req.method} to ${
+        req.url
+      } at [${new Date().toISOString()}]`
+    );
 
-};
+    next();
+  };
+}
+
+server.use(helmet());
+server.use(express.json());
+server.use(morgan("dev"));
+// server.use(logger);
+
+server.get("/", logger(" Logger on /"), (req, res) => {
+  // res.send(`<h2>Let's write some middleware!</h2>`);
+  res.status(200).json({ message: "logger is on" });
+});
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,39 @@
-const express = 'express';
+const express = require("express");
 
 const router = express.Router();
+const userDb = require("./userDb");
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {});
 
-});
+router.post("/:id/posts", (req, res) => {});
 
-router.post('/:id/posts', (req, res) => {
+router.get("/", (req, res) => {});
 
-});
+router.get("/:id", (req, res) => {});
 
-router.get('/', (req, res) => {
+router.get("/:id/posts", (req, res) => {});
 
-});
+router.delete("/:id", (req, res) => {});
 
-router.get('/:id', (req, res) => {
-
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", (req, res) => {});
 
 //custom middleware
 
 function validateUserId(req, res, next) {
+  let { id } = req.params;
+  if (id) {
+    id = req.user;
+  } else if (!id) {
+  }
 
-};
+  next();
+}
 
-function validateUser(req, res, next) {
+function validateUser(req, res, next) {}
 
-};
-
-function validatePost(req, res, next) {
-
-};
+function validatePost(req, res, next) {}
 
 module.exports = router;
+// module.exports = validateUserId;
+// module.exports = validateUser;
+// module.exports = validatePost;
